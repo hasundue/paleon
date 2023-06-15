500d040775022a868b0925e03c972d6daa12215a
hasundue <hasundue@gmail.com>
2023-06-15 23:57:38 +0900

    Create Home

--- routes/index.tsx
+++ routes/index.tsx
@@ -7,6 +7,31 @@ export default function Home() {
       <Head />
       <body>
         <Header current="Home" />
+
+        <div style="text-align: center">
+          <h1>
+            Paleon
+          </h1>
+          <p>
+            A logging service and library with <b>Deno Deploy</b> and{" "}
+            <b>Deno KV</b>.
+          </p>
+        </div>
+
+        <div style="display: flex; justify-content: center; align-items: center;">
+          <img src="/logo.png" alt="Paleon Logo" />
+        </div>
+
+        <section>
+          <div style="text-align: center">
+            <p>
+              Developed for{" "}
+              <a href="https://github.com/denoland/deno-kv-hackathon">
+                Deno KV Hackathon
+              </a>
+            </p>
+          </div>
+        </section>
       </body>
     </>
   );
