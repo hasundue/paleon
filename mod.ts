a22ac5314a925d818c74d9533f51cc6cf7f3d6fa
hasundue <hasundue@gmail.com>
2023-06-15 09:59:42 +0900

    Rename `Log` to `Paleon` and `LogRecord` to `PaleonRecord`

--- mod.ts
+++ mod.ts
@@ -1,49 +1,54 @@
-export type Log<T = unknown> = {
+import { BaseHandler, HandlerOptions } from "$std/log/handlers.ts";
+import { type LevelName } from "$std/log/levels.ts";
+import { type LogRecord } from "$std/log/logger.ts";
+
+export type Paleon<T extends PaleonRecord = PaleonRecord> = {
   readonly subject: Deno.KvKey | Deno.KvKeyPart;
-  read(options?: ReadOptions): ReadableStream<LogRecord<T>>;
-  write(value: T, options?: WriteOptions): Promise<Deno.KvCommitResult>;
+  read(options?: ReadOptions): ReadableStream<T>;
+  write(value: T): Promise<Deno.KvCommitResult>;
   erase(): Promise<void>;
   close(): void;
 };
 
-export type ReadOptions = { since?: Date; until?: Date; limit?: number };
-export type WriteOptions = { time?: number };
+export interface PaleonRecord {
+  datetime: Date;
+}
 
-export type LogRecord<T = unknown> = {
-  time: number;
-  body: T;
+export type ReadOptions = {
+  since?: Date;
+  until?: Date;
+  limit?: number;
+  reverse?: boolean;
 };
 
-export const Log = {
-  async open<T = unknown>(
+export const Paleon = {
+  async open<T extends PaleonRecord>(
     subject: Deno.KvKey | Deno.KvKeyPart,
-  ): Promise<Log<T>> {
+  ): Promise<Paleon<T>> {
     const prefix = [subject].flat();
     const kv = await Deno.openKv();
 
     return {
       subject,
 
-      write(value: T, options?: WriteOptions) {
-        const time = options?.time ?? Date.now();
+      write(record: T) {
+        const time = record.datetime.getTime();
         const key = [...prefix, time, crypto.randomUUID()];
-        return kv.set(key, value);
+        return kv.set(key, record);
       },
 
       read(options?: ReadOptions) {
         const start = [...prefix, options?.since?.getTime() ?? 0];
         const end = [...prefix, options?.until?.getTime() ?? Infinity];
         const limit = options?.limit ?? 10;
+        const reverse = options?.reverse ?? true;
 
-        const iter = kv.list<T>({ start, end }, { limit, reverse: true });
+        const iter = kv.list<T>({ start, end }, { limit, reverse });
 
-        return new ReadableStream<LogRecord<T>>({
+        return new ReadableStream<T>({
           async start(controller) {
-            for await (const { key, value } of iter) {
-              controller.enqueue({
-                time: Number(key[prefix.length]),
-                body: value,
-              });
+            for await (const { value } of iter) {
+              controller.enqueue(value);
             }
             controller.close();
           },
@@ -62,3 +67,24 @@ export const Log = {
     };
   },
 };
+
+export class PaleonHandler extends BaseHandler {
+  readonly #_paleon: Paleon;
+
+  constructor(paleon: Paleon, levelName: LevelName, options?: HandlerOptions) {
+    super(levelName, options);
+    this.#_paleon = paleon;
+  }
+
+  static async init<T extends LogRecord = LogRecord>(
+    levelName: LevelName,
+    options?: HandlerOptions,
+  ) {
+    const paleon = await Paleon.open<T>("logs");
+    return new PaleonHandler(paleon, levelName, options);
+  }
+
+  override handle(logRecord: LogRecord): Promise<Deno.KvCommitResult> {
+    return this.#_paleon.write(logRecord);
+  }
+}
--- test/mod_test.ts
+++ test/mod_test.ts
@@ -1,62 +1,56 @@
-import {
-  assert,
-  assertEquals,
-} from "https://deno.land/std@0.191.0/testing/asserts.ts";
-import {
-  afterAll,
-  beforeAll,
-  describe,
-  it,
-} from "https://deno.land/std@0.191.0/testing/bdd.ts";
-import { collect } from "https://deno.land/x/streamtools@v0.5.0/mod.ts";
-import { Log } from "../mod.ts";
+import { assert, assertEquals } from "$std/testing/asserts.ts";
+import { afterAll, beforeAll, describe, it } from "$std/testing/bdd.ts";
+import { collect } from "$streamtools/mod.ts";
+import { Paleon } from "/mod.ts";
 
-describe("Log", () => {
-  let log: Log<string>;
+type TestLogRecord = {
+  datetime: Date;
+  msg: string;
+};
+
+describe("Paleon", () => {
+  let paleon: Paleon<TestLogRecord>;
 
   beforeAll(async () => {
-    log = await Log.open("test");
-    await log.erase();
+    paleon = await Paleon.open<{ datetime: Date; msg: string }>("mod_test");
+    await paleon.erase();
   });
 
   afterAll(() => {
-    log.close();
+    paleon.close();
   });
 
   it("should open a log", () => {
-    assert(log);
+    assert(paleon);
   });
 
   it("should write a record", async () => {
-    await log.write("hello");
+    await paleon.write({ datetime: new Date(), msg: "hello" });
   });
 
   it("should read a record", async () => {
-    const records = await collect(log.read());
+    const records = await collect(paleon.read());
     assertEquals(records.length, 1);
-    assertEquals(records[0].body, "hello");
-  });
-
-  it("should write records with a timestamp", async () => {
-    await log.write("hello", { time: 0 });
-    await log.write("hello", { time: 1 });
+    assertEquals(records[0].msg, "hello");
   });
 
   it("should read records with a time range", async () => {
+    await paleon.write({ datetime: new Date(1), msg: "1" });
+    await paleon.write({ datetime: new Date(2), msg: "2" });
     const records = await collect(
-      log.read({ since: new Date(0), until: new Date(1) }),
+      paleon.read({ since: new Date(0), until: new Date(3) }),
     );
-    assertEquals(records.length, 1);
+    assertEquals(records.length, 2);
   });
 
-  it("should read multiple records", async () => {
-    const records = await collect(log.read());
+  it("should read all records", async () => {
+    const records = await collect(paleon.read());
     assertEquals(records.length, 3);
   });
 
   it("should erase a log", async () => {
-    await log.erase();
-    const records = await collect(log.read());
+    await paleon.erase();
+    const records = await collect(paleon.read());
     assertEquals(records.length, 0);
   });
 });
